48f9cbed5fd7575d5bfb8e8f5e7ad54cbd805e2e
MikaSez <mis.strator@gmail.com>
2015-10-04 19:16:45 +0200

    Init project

---
+++ src/main/java/info/mikasez/R7ConverterApplication.java
@@ -0,0 +1,12 @@
+package info.mikasez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R7ConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(R7ConverterApplication.class, args);
+    }
+}
