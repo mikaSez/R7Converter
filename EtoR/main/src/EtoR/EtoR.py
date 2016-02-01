156da28339eac92c70047f3b715fa1e74b4b9828
MikaSez <mis.strator@gmail.com>
2016-02-01 13:12:33 +0100

    R7Converter-17 Ecrire un script qui copie les dépendances Reveal

--- EtoR/main/src/EtoR/EtoR.py
+++ EtoR/main/src/EtoR/EtoR.py
@@ -1,34 +1,60 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+from shutil import copy2, rmtree
 from tkinter import *
 from tkinter.filedialog import askopenfilename, asksaveasfilename
+import os.path
+
+from os import mkdir
+from pathlib import Path
+
+resources_path = os.path.join(os.path.split(__file__)[0], "resources")
+
 
 window = Tk()
 
 entry = StringVar("")
 exit = StringVar("")
 
+def copytree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            if not os.path.isdir(d):
+                mkdir(d)
+            copytree(s, d, symlinks, ignore)
+        else:
+            copy2(s, d)
+
+
 def openFileName():
     entry.set(askopenfilename(title="Choose your EAST file", filetypes=[('xml file', '.xml')]))
     exit.set(entry.get().replace(".xml", ".html"))
 
 def saveAs():
-    entry.set(asksaveasfilename(title="Specify exit file", filetypes=[('html file', '.html')]))
+    exit.set(asksaveasfilename(title="Specify exit file", filetypes=[('html file', '.html')]))
+
+def convert():
+    exitPath = str(Path(exit.get()).parent)
+    if(os.path.isdir(exitPath+"/revealApp")):
+        rmtree(exitPath +"/revealApp")
+    copytree(resources_path, exitPath)
 
 Label(window, text="Converter EtoR").grid(row=1, column=1)
-Entry(window, textvariable=entry, width=50).grid(row=2, column=1)
+
+Entry(window, textvariable=entry, width=50, state="readonly").grid(row=2, column=1)
 Button(window, text="Chose entry", command=openFileName, width=30).grid(row=2, column=2)
 
 
 
-Entry(window, textvariable=exit, width=50).grid(row=3, column=1)
+Entry(window, textvariable=exit, width=50, state="readonly").grid(row=3, column=1)
 Button(window, text="Chose exit directory (default : same)", command=saveAs, width=30).grid(row=3, column=2)
 
 
 Checkbutton(window, text="Override").grid(row=4, column=1)
 Button(window, text="Close", command=window.quit, width=20).grid(row=5, column=1)
-Button(window, text="Run", width=20).grid(row=5, column=2)
+Button(window, text="Run", command=convert, width=20).grid(row=5, column=2)
 window.mainloop()
 
 
---
+++ EtoR/main/src/EtoR/__init__.py
@@ -0,0 +1,3 @@
+import os.path
+resource_path = os.path.join(os.path.split(__file__)[0], "resources")
+
